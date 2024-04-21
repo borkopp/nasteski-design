728e204ba7fb51777a1d3c94f7ec6a4e61a7d47d
Borko Petrevski <borkop.de@gmail.com>
2024-04-22 01:46:11 +0200

    updated background iamge

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,16 @@
   <div class="leading-normal tracking-normal text-black bg-gradient-to-r from-slate-50 to-slate-300 ">
     <LayoutNav />
 
-    <div data-aos="fade-up" class="pt-24">
-      <div class="container xl:mx-0 xl:max-w-none px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+    <div id="home" data-aos="fade-up" class="pt-24">
+      <div class="container lg:h-[80vh] px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+
         <div
           class="flex flex-col w-full justify-center items-center lg:items-start lg:w-2/5 lg:text-left lg:pl-10  text-center">
-          <h1 class="text-4xl sm:text-4xl xl:text-7xl md:text-5xl font-bold leading-tight mb-8 lg:text-6xl">
+          <h1
+            class="text-4xl sm:text-4xl xl:text-6xl text-white md:text-black md:text-5xl font-bold leading-tight mb-8 lg:text-6xl">
             Алуминиумски <br /> огради & тенди
           </h1>
-          <p class="text-lg md:text-xl xl:text-2xl leading-normal mb-4 mx-4 md:mx-0 lg:mb-8">
+          <p class="text-lg md:text-xl xl:text-2xl leading-normal mb-4 mx-4 md:mx-0 text-white md:text-black lg:mb-8">
             Професионална изработка на алуминиумски <br class="hidden xl:block" /> огради и тенди по мера од различни
             профили <br class="hidden xl:block" />
             на
@@ -27,36 +29,36 @@
 
 
         </div>
-        <div class="w-full hidden lg:block md:w-3/5 pb-20">
+        <!-- <div class="w-full hidden lg:block md:w-3/5 pb-20">
           <img class="w-full mx-auto md:w-4/6 justify-center" src="../assets/imgs/hero.png" />
-        </div>
+        </div> -->
       </div>
+      <div class="hidden md:block relative -mt-12 lg:-mt-24">
+        <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+              <path
+                d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
+                opacity="0.100000001"></path>
+              <path
+                d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+                opacity="0.100000001"></path>
+              <path
+                d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
+                id="Path-4" opacity="0.200000003"></path>
+            </g>
+            <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+              <path
+                d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z">
+              </path>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
 
-    </div>
-    <div class="relative -mt-12 lg:-mt-24">
-      <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
-            <path
-              d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
-              opacity="0.100000001"></path>
-            <path
-              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-              opacity="0.100000001"></path>
-            <path
-              d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
-              id="Path-4" opacity="0.200000003"></path>
-          </g>
-          <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
-            <path
-              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z">
-            </path>
-          </g>
-        </g>
-      </svg>
-    </div>
-    <section id="about" class="bg-white border-b py-8">
+    <section id="about" class="bg-white border-b py-8 pt-10">
       <div class="container  max-w-5xl mx-auto m-8">
         <h2 data-aos="fade-up"
           class="w-full my-2 xl:text-6xl text-5xl font-bold leading-tight text-center text-gray-800">
@@ -154,7 +156,7 @@
 
       </div>
     </section>
-    <section id="gallery" class="bg-gray-100 py-8">
+    <section id="gallery" class="bg-gray-100 py-8 pt-12">
       <div class="container mx-auto px-2 pt-4 pb-12 text-gray-800">
         <h2 data-aos="fade-up"
           class="w-full my-2 text-5xl xl:text-6xl font-bold leading-tight text-center text-gray-800">
@@ -548,6 +550,28 @@ onMounted(() => {
     transition: transform 0.3s ease-in-out;
     transform: scale(1.05);
   }
+}
 
+#home {
+  background-image: url('../assets/imgs/home-background.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+
+@media (max-width: 768px) {
+  #home {
+    background-size: auto;
+    /* Adjust the height as needed */
+    height: 70vh;
+    /* blur effect */
+    background-color: rgba(0, 0, 0, 0.6);
+    background-blend-mode: overlay;
+    background-image: url('../assets/imgs/home-background.png');
+    background-size: cover;
+    background-position: right;
+    background-repeat: no-repeat;
+  }
 }
 </style>
